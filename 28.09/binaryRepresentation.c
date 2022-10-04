51a44bd3ac577809b114ebedcda42097b6211d39
IgnatSergeev <ignatii.f.sergeev@gmail.com>
2022-10-04 21:02:51 +0300

    Added tests

--- 28.09/binaryRepresentation.c
+++ 28.09/binaryRepresentation.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <math.h>
+#include <stdbool.h>
 
 void representTheNumberInBinary(int number, int numberBinaryRepresentation[]) {
     if (number < 0) {
@@ -44,7 +45,66 @@ int binaryRepresentationToDecimal(const int numberBinaryRepresentation[]) {
     return number;
 }
 
+bool test() {
+    bool typicalTest = true;
+
+    int *test1NumberBinaryRepresentation = (int*)malloc(RegistrySize * sizeof(int));
+    if (test1NumberBinaryRepresentation == NULL) {
+        printf("Проблемы с аллокацией");
+
+        return false;
+    }
+    int *test2NumberBinaryRepresentation = (int*)malloc(RegistrySize * sizeof(int));
+    if (test2NumberBinaryRepresentation == NULL) {
+        printf("Проблемы с аллокацией");
+
+        free(test1NumberBinaryRepresentation);
+        return false;
+    }
+    representTheNumberInBinary(10, test1NumberBinaryRepresentation);
+    representTheNumberInBinary(-2, test2NumberBinaryRepresentation);
+    int correctTest1[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0};
+    int correctTest2[] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0};
+    for (int i = 0; i < RegistrySize; i++) {
+        if (correctTest1[i] != test1NumberBinaryRepresentation[i] || correctTest2[i] != test2NumberBinaryRepresentation[i]) {
+            typicalTest = false;
+        }
+    }
+
+    int *test3SumBinaryRepresentation = (int*)malloc(RegistrySize * sizeof(int));
+    if (test3SumBinaryRepresentation == NULL) {
+        printf("Проблемы с аллокацией");
+
+        free(test1NumberBinaryRepresentation);
+        free(test2NumberBinaryRepresentation);
+        return false;
+    }
+    int correctTest3[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0};
+    sumTwoBinaries(test1NumberBinaryRepresentation, test2NumberBinaryRepresentation, test3SumBinaryRepresentation);
+    for (int i = 0; i < RegistrySize; i++) {
+        if (correctTest3[i] != test3SumBinaryRepresentation[i]) {
+            typicalTest = false;
+        }
+    }
+    if (binaryRepresentationToDecimal(test3SumBinaryRepresentation) != 8) {
+        typicalTest = false;
+    }
+
+    free(test3SumBinaryRepresentation);
+    free(test2NumberBinaryRepresentation);
+    free(test1NumberBinaryRepresentation);
+    return typicalTest;
+}
+
 int main() {
+    if (!test()) {
+        printf("Тесты провалены");
+
+        return -1;
+    } else {
+        printf("Тесты пройдены\n");
+    }
+
     int firstNumber = 0;
     int secondNumber = 0;
     printf("Введите числа по модулю меньшие 2^%d\n", RegistrySize - 1);
