5c6941fbd27ac4ad8ee0678cd4cdbe327149a6c0
IgnatSergeev <ignatii.f.sergeev@gmail.com>
2022-10-04 21:53:52 +0300

    Added qsort module

---
+++ 28.09/modulesAndFiles/customQSort.h
@@ -0,0 +1,9 @@
+#pragma once
+
+int median(int first, int second, int third);
+
+void insertSort(int inputArray[], int lowBorder, int highBorder);
+
+void halfQsort(int array[], int lowBound, int highBound);
+
+void qSort(int array[], int lowBorder, int highBorder);
