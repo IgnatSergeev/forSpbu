62f4e5e635fad8671d8e049d0746f29b0b46c31e
IgnatSergeev <ignatii.f.sergeev@gmail.com>
2022-10-04 19:09:11 +0300

    Added binary representation

--- 28.09/binaryRepresentation.c
+++ 28.09/binaryRepresentation.c
@@ -2,27 +2,47 @@
 #include <stdbool.h>
 #include <malloc.h>
 #include <time.h>
+#include <math.h>
 
-bool test() {
-    bool typicalTest = true;
-
-    return typicalTest;
-}
-
-int main() {
-    if (!test()) {
-        printf("Tests failed");
-
-        return -1;
-    } else {
-        printf("Tests passed\n");
+void representTheNumberInBinary(int number, int numberBinaryRepresentation[]) {
+    if (number < 0) {
+        number = (int)pow(2,31) + 1 + number;
     }
 
+    for (int i = 0; i < 31; i++) {
+        int bit = number & 1;
+        number = number >> 1;
+        numberBinaryRepresentation[30 - i] = bit;
+    }
+}
+
+
+int main() {
     int firstNumber = 0;
     int secondNumber = 0;
-    printf("%s", "Enter the numbers\n");
+    printf("%s", "Введите числа по модулю меньшие 2^30\n");
     scanf("%d", &firstNumber);
     scanf("%d", &secondNumber);
+    //проверка что действительно меньше 30
 
+    int *firstNumberBinaryRepresentation = (int*)malloc(31 * sizeof(int));
+    if (firstNumberBinaryRepresentation == NULL) {
+        printf("Error with allocation");
+
+        return -1;
+    }
+    int *secondNumberBinaryRepresentation = (int*)malloc(31 * sizeof(int));
+    if (secondNumberBinaryRepresentation == NULL) {
+        printf("Error with allocation");
+
+        return -1;
+    }
+
+    representTheNumberInBinary(firstNumber, firstNumberBinaryRepresentation);
+    for (int i = 0; i < 31; i++) {
+        printf("%d", firstNumberBinaryRepresentation[i]);
+    }
+    free(firstNumberBinaryRepresentation);
+    free(secondNumberBinaryRepresentation);
     return 0;
 }
