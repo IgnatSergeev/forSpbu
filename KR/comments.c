4ae8047c24bda34f9fb1be1c7f69b9cbab9d2fa4
IgnatSergeev/laptop <ignatii.f.sergeev@gmail.com>
2022-10-05 16:16:33 +0300

    Task 3 is done(only tests left)

--- KR/comments.c
+++ KR/comments.c
@@ -28,14 +28,28 @@ int main() {
 
         return -1;
     }
-    char line[101] = {0};
+
+    const int lineSize = 100;
+    char line[100] = {0};
     while (!feof(file)) {
         if (fgets((char *) &line, 100, file) == NULL) {
             printf("Возникла ошибка при чтении\n");
 
             return -1;
         }
-        printf(line);
-    }
 
+        int curIndex = 0;
+        while (curIndex < lineSize && line[curIndex] != '\0' && line[curIndex] != ';') {
+            ++curIndex;
+        }
+        if (curIndex < lineSize && line[curIndex] != '\0') {
+            for (int i = curIndex; i < lineSize; i++) {
+                if (line[i] == '\0') {
+                    break;
+                }
+
+                printf("%c", line[i]);
+            }
+        }
+    }
 }
