30bcc73b1a88c8427dcc9d5033d0323ab67730e8
IgnatSergeev <ignatii.f.sergeev@gmail.com>
2022-10-22 20:14:38 +0300

    Fixed error of popping null stack

--- Modules/customStack.c
+++ Modules/customStack.c
@@ -25,7 +25,7 @@ int push(Stack *stack, Type value) {
 }
 
 Type pop(Stack *stack, int *errorCode) {
-    if (stack == NULL) {
+    if (isEmpty(stack)) {
         if (errorCode != NULL) {
             *errorCode = -1;
         }
