b78bc43b4b75e667be4cabe9780764dd0ee166e8
IgnatSergeev <ignatii.f.sergeev@gmail.com>
2022-10-22 21:57:08 +0300

    Second way with expression struct should be working but it doesnt - need to ask

--- 12.10/postfixCalculator.c
+++ 12.10/postfixCalculator.c
@@ -1,7 +1,14 @@
 #include "../Modules/customStack.h"
 #include <stdio.h>
+#include <malloc.h>
 
 #define maxLineSize 100
+
+typedef struct Expression{
+    char *firstArgument;
+    char *secondArgument;
+    char operation;
+} Expression;
 
 bool isDigit(char element) {
     if (element == '0' || element == '1' || element == '2' || element == '3' || element == '4' || element == '5'
@@ -11,78 +18,155 @@ bool isDigit(char element) {
     return false;
 }
 
-void evaluateStack(Stack *stack, int *returnErrorCode, Stack *operations) {
-    if (isEmpty(stack)) {
-        return;
-    }
-    char *topElement = pop(stack, returnErrorCode);
-    if (*returnErrorCode) {
-        return;
-    }
-    int topElementSize = 0;
-    while (topElement[topElementSize + 1] != '\0') {
-        ++topElementSize;
+Expression *parseStringIntoExpression(char *string, int *errorCode) {
+    *errorCode = 0;
+    int stringSize = 0;
+    while (string[stringSize + 1] != '\0') {
+        ++stringSize;
     }
 
-    if (isDigit(topElement[topElementSize])) {
-        evaluateStack(stack, returnErrorCode, operations);
-        if (*returnErrorCode) {
-            return;
+    stringSize += 1;
+    char operation = string[stringSize - 1];
+
+    int numOfDigits = 1;
+    char firstArgument[maxLineSize] = {0};
+    int firstArgumentEndIndex = 0;
+    char secondArgument[maxLineSize] = {0};
+    int secondArgumentStartIndex = 0;
+
+    for (int i = stringSize - 3; i >= 0; i--) {
+        if (string[i] == ' ') {
+            continue;
         }
-        push(stack, topElement);
+        if (isDigit(string[i])) {
+            numOfDigits -= 1;
+            if (numOfDigits == 0) {
+                firstArgumentEndIndex = i - 2;
+                secondArgumentStartIndex = i;
+                break;
+            }
+            continue;
+        }
+        numOfDigits += 1;
+    }
+
+    for (int i = 0; i <= firstArgumentEndIndex; i++) {
+        firstArgument[i] = string[i];
+    }
+    int secondArgumentIndex = 0;
+    for (int i = secondArgumentStartIndex; i < stringSize - 2; i++) {
+        secondArgument[secondArgumentIndex] = string[i];
+        ++secondArgumentIndex;
+    }
+
+    Expression *expression = malloc(sizeof (Expression));
+    if (expression == NULL) {
+        printf("Ошибка выделения памяти\n");
+        *errorCode = -1;
+        return NULL;
+    }
+    expression->firstArgument = firstArgument;
+    expression->secondArgument = secondArgument;
+    expression->operation = operation;
+    return expression;
+}
+
+int evaluateExpression(Expression *expression, int *errorCode) {
+    *errorCode = 0;
+    int indexOfFirstArgument = 0;
+    int indexOfSecondArgument = 0;
+    while (expression->firstArgument[indexOfFirstArgument + 1] != '\0') {
+        ++indexOfFirstArgument;
+    }
+    while (expression->secondArgument[indexOfSecondArgument + 1] != '\0') {
+        ++indexOfSecondArgument;
+    }
+
+    int valueOfFirstArgument = 0;
+    int valueOfSecondArgument = 0;
+    if (isDigit(expression->firstArgument[indexOfFirstArgument]) && isDigit(expression->secondArgument[indexOfSecondArgument])) {
+        valueOfFirstArgument = ((int)expression->firstArgument[indexOfFirstArgument] - 50);
+        valueOfSecondArgument = ((int)expression->secondArgument[indexOfSecondArgument] - 50);
     } else {
-        topElementSize += 1;
-        char operation = topElement[topElementSize - 1];
-        push(operations, &operation);
-        int numOfDigits = 1;
-        char firstArgument[maxLineSize] = {0};
-        int firstArgumentEndIndex = 0;
-        char secondArgument[maxLineSize] = {0};
-        int secondArgumentStartIndex = 0;
-
-        for (int i = topElementSize - 3; i >= 0; i--) {
-            if (topElement[i] == ' ') {
-                continue;
-            }
-            if (isDigit(topElement[i])) {
-                numOfDigits -= 1;
-                if (numOfDigits == 0) {
-                    firstArgumentEndIndex = i - 2;
-                    secondArgumentStartIndex = i;
-                    break;
-                }
-                continue;
-            }
-            numOfDigits += 1;
+        Expression *firstArgumentExpression = parseStringIntoExpression(expression->firstArgument, errorCode);
+        if (*errorCode) {
+            return 0;
         }
-
-        for (int i = 0; i <= firstArgumentEndIndex; i++) {
-            firstArgument[i] = topElement[i];
+        Expression *secondArgumentExpression = parseStringIntoExpression(expression->secondArgument, errorCode);
+        if (*errorCode) {
+            return 0;
         }
-        int secondArgumentIndex = 0;
-        for (int i = secondArgumentStartIndex; i < topElementSize - 2; i++) {
-            secondArgument[secondArgumentIndex] = topElement[i];
-            ++secondArgumentIndex;
+        valueOfFirstArgument = evaluateExpression(firstArgumentExpression, errorCode);
+        if (*errorCode) {
+            return 0;
         }
-
-        push(stack, firstArgument);
-        push(stack, secondArgument);
-        evaluateStack(stack, returnErrorCode, operations);
-        if (*returnErrorCode) {
-            return;
+        valueOfSecondArgument = evaluateExpression(secondArgumentExpression, errorCode);
+        if (*errorCode) {
+            return 0;
         }
     }
+
+    if (expression->operation == '+') {
+        return valueOfFirstArgument + valueOfSecondArgument;
+    }
+    if (expression->operation == '-') {
+        return valueOfFirstArgument - valueOfSecondArgument;
+    }
+    if (expression->operation == '*') {
+        return valueOfFirstArgument * valueOfSecondArgument;
+    }
+    if (expression->operation == '/') {
+        return valueOfFirstArgument / valueOfSecondArgument;
+    }
+    printf("Незивестная операция\n");
+    *errorCode = -1;
+    return 0;
+}
+
+bool test() {
+    return true;
 }
 
 int main() {
-    Stack *stack = createStack();
-    Stack *operations = createStack();
-    push(stack, "9 6 - 1 2 + *");
-    int errorCode = 0;
-    evaluateStack(stack, &errorCode, operations);
-    if (errorCode) {
-        printf("Возникла ошибка в функции подсчёта стэка");
+    if (!test()) {
+        printf("Тесты не пройдены\n");
+
+        return -1;
+    } else {
+        printf("Тесты пройдены\n");
+    }
+
+    printf("Введите арифметическое выражение в постфиксной форме(в нём могут учавствовать цифры и знаки + - * /) не диннее 100 символов и перевод строки\n");
+    char arithmeticExpression[maxLineSize] = {0};
+    int arithmeticExpressionIndex = 0;
+    char stringElement = 0;
+    while (scanf("%c", &stringElement)) {
+        if (arithmeticExpressionIndex >= maxLineSize || stringElement == '\n') {
+            break;
+        }
+        arithmeticExpression[arithmeticExpressionIndex] = stringElement;
+        ++arithmeticExpressionIndex;
+    }
+    if (arithmeticExpressionIndex >= maxLineSize) {
+        printf("%s%d%s", "Введённое выражение длиннее " , maxLineSize," символов");
         return -1;
     }
+
+    int errorCode = 0;
+    Expression *expression = {parseStringIntoExpression(arithmeticExpression, &errorCode);
+    if (errorCode) {
+        printf("Произошла ошибка при парсинге выражения\n");
+        return -1;
+    }
+
+    printf("%c\n%s\n%s\n", expression->operation, expression->firstArgument, expression->secondArgument);
+
+    int result = evaluateExpression(expression, &errorCode);
+    if (errorCode) {
+        printf("Произошла ошибка при вычеслении выражения\n");
+        return -1;
+    }
+
+    printf("%d", result);
     return 0;
 }
--- 12.10/typeDef.h
+++ 12.10/typeDef.h
@@ -1,3 +1,3 @@
 #pragma once
 
-typedef char* Type;
+typedef char Type;
