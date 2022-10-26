e1659ffda1a2cc60aca130680b90eec7a06a6b31
IgnatSergeev/laptop <ignatii.f.sergeev@gmail.com>
2022-10-26 18:14:10 +0300

    Task 2 is done need to add tests

---
+++ KR2/Task_2.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <locale.h>
+#include <malloc.h>
+#include <time.h>
+
+#define max_Line_Size 100
+
+void randomlySwapTwoElements(int array[], int arraySize) {
+    int firstRandomIndex = rand() % arraySize;
+    int secondRandomIndex = rand() % arraySize;
+    while (firstRandomIndex == secondRandomIndex) {
+        secondRandomIndex = rand() % arraySize;
+    }
+    int temp = array[firstRandomIndex];
+    array[firstRandomIndex] = array[secondRandomIndex];
+    array[secondRandomIndex] = temp;
+}
+
+bool isSorted(const int array[], int arraySize) {
+    for (int i = 0; i < arraySize - 1; i++) {
+        if (array[i] > array[i+1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void monkeySort(int array[], int arraySize) {
+    int currentIteration = 0;
+    while (!isSorted(array, arraySize)) {
+        ++currentIteration;
+        randomlySwapTwoElements(array, arraySize);
+        printf("Текущая итерация = %d\n", currentIteration);
+    }
+}
+
+bool test() {
+    bool testResult = true;
+
+    return testResult;
+}
+
+int main() {
+    setlocale(LC_ALL, "");
+
+    if (!test()) {
+        printf("Тесты провалены");
+
+        return -1;
+    } else {
+        printf("Тесты пройдены\n");
+    }
+
+    printf("Введите количество элементов массива,а затем сами элементы\n");
+    int numOfArrayElements = 0;
+    scanf("%d", &numOfArrayElements);
+    if (numOfArrayElements <= 0) {
+        printf("Размер массива должен быть положительным\n");
+        return -1;
+    }
+    int *array = calloc(numOfArrayElements, sizeof(int));
+    if (array == NULL) {
+        printf("Проблемы с аллокацией памяти\n");
+        return -1;
+    }
+    for (int i = 0; i < numOfArrayElements; i++) {
+        int number = 0;
+        scanf("%d", &number);
+        array[i] = number;
+    }
+
+    srand((unsigned)(time(NULL)));
+    monkeySort(array, numOfArrayElements);
+    printf("Массив отсортирован\n");
+    return 0;
+}
