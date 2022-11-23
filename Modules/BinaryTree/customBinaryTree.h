5b2dbee0b6eeac70d149d87b376abcb4435036fa
IgnatSergeev/laptop <ignatii.f.sergeev@gmail.com>
2022-11-23 16:39:50 +0300

    Added comments

--- Modules/BinaryTree/customBinaryTree.h
+++ Modules/BinaryTree/customBinaryTree.h
@@ -2,10 +2,11 @@
 
 #include <stdbool.h>
 #include <stdlib.h>
-#include "../typeDef.h"
+#include "typeDef.h"
 
 typedef struct BinaryTree BinaryTree;
 
+//creates an empty binary tree
 BinaryTree *create();
 
 //compare: should return 0 if equal, 1 if first is grater than second and -1 if first is less than second
@@ -13,10 +14,16 @@ BinaryTree *create();
 int addValue(BinaryTree *tree, Type value, int (*compare)(Type, Type), Type (*whatIfEqualInAdding)(Type, Type));
 
 //whatIfEqualInSearching: first value - is old value; second value - is new value
+//compare: should return 0 if equal, 1 if first is grater than second and -1 if first is less than second
+//zeroValue - the value which is returned in case the search is failed
 Type findValue(BinaryTree *tree, Type value, int *errorCode, Type zeroValue, int (*compare)(Type, Type), Type (*whatIfEqualInSearching)(Type, Type));
 
+//clears the tree(cannot be used after cleaning)
 void clear(BinaryTree *tree);
 
+//deletes element from the tree by value
+//compare: should return 0 if equal, 1 if first is grater than second and -1 if first is less than second
 int deleteValue(BinaryTree *tree, Type value, int (*compare)(Type, Type));
 
+//checks id the tree is empty
 bool isEmpty(BinaryTree *tree);
