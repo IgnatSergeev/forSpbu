69227bf959a97d36f9429ba3fa4ce7edd558b6e3
IgnatSergeev/laptop <ignatii.f.sergeev@gmail.com>
2022-12-07 18:39:40 +0300

    Added comments in queue.h

--- Modules/queue.h
+++ Modules/queue.h
@@ -11,12 +11,17 @@ typedef struct Queue{
     struct Node* tail;
 } Queue;
 
+//Adds element to the top of the queue
 int enqueue(Queue *queue, int element);
 
+//Removes element to the bottom of the queue
 int dequeue(Queue *queue, int *errorCode);
 
+//Checks if the queue is empty
 bool isEmpty(Queue *queue);
 
+//Clears the queue - cannot be used again
 void clear(Queue *queue);
 
+//Creates empty queue
 Queue *createQueue();
