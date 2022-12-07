368110f59dfe6b02b5526345ff5946b5f4750436
IgnatSergeev/laptop <ignatii.f.sergeev@gmail.com>
2022-12-07 17:22:24 +0300

    Init commit

---
+++ KR5/rewrite.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "../Modules/BinaryTree/customBinaryTree.h"
+
+int main(void) {
+    if (test()) {
+        printf("Тесты пройдены\n");
+    } else {
+        printf("Тесты провалены\n");
+        return -1;
+    }
+
+    int leftBorder = 0;
+    int rightBorder = 0;
+    printf("Введите правую и левую границы отрезка, который требуется для задачи\n");
+    scanf("%d", &leftBorder);
+    scanf("%d", &rightBorder);
+
+    List
+    FILE *inputFile = fopen("rewriteInput.txt", "r");
+
+
+    return 0;
+}
