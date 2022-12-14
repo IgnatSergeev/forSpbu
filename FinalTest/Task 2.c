6fdcad0a7cd3993da6c92d589ff99eef8f5ca5c6
IgnatSergeev/laptop <ignatii.f.sergeev@gmail.com>
2022-12-14 16:45:51 +0300

    Added tests for this function

--- FinalTest/Task 2.c
+++ FinalTest/Task 2.c
@@ -1,10 +1,97 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
+#include "../Modules/ListModule/customList.h"
 
 bool test() {
     bool testResult = true;
+    List *list = create();
+    if (list == NULL) {
+        printf("Ошибка при создании списка\n");
+        return false;
+    }
 
+    int errorCode =insertNodeToEnd(list, "abc");
+    if (errorCode) {
+        printf("Возникла ошибка при добавлении ноды\n");
+        return false;
+    }
+    errorCode = insertNodeToEnd(list, "");
+    if (errorCode) {
+        printf("Возникла ошибка при добавлении ноды\n");
+        return false;
+    }
+    errorCode = insertNodeToEnd(list, "acd");
+    if (errorCode) {
+        printf("Возникла ошибка при добавлении ноды\n");
+        return false;
+    }
+    errorCode = insertNodeToEnd(list, "bcd");
+    if (errorCode) {
+        printf("Возникла ошибка при добавлении ноды\n");
+        return false;
+    }
+
+    errorCode = appendToEndStringsStartingWithA(list);
+    if (errorCode) {
+        printf("Возникла ошибка в вызове функции\n");
+        return false;
+    }
+
+    Type value = deleteLastNode(list);
+    if (value == NULL) {
+        printf("Возникла ошибка при удалении ноды\n");
+        return false;
+    }
+    if (strcmp(value, "acd")) {
+        testResult = false;
+    }
+
+    value = deleteLastNode(list);
+    if (value == NULL) {
+        printf("Возникла ошибка при удалении ноды\n");
+        return false;
+    }
+    if (strcmp(value, "abc")) {
+        testResult = false;
+    }
+
+    value = deleteLastNode(list);
+    if (value == NULL) {
+        printf("Возникла ошибка при удалении ноды\n");
+        return false;
+    }
+    if (strcmp(value, "bcd")) {
+        testResult = false;
+    }
+
+    value = deleteLastNode(list);
+    if (value == NULL) {
+        printf("Возникла ошибка при удалении ноды\n");
+        return false;
+    }
+    if (strcmp(value, "acd")) {
+        testResult = false;
+    }
+
+    value = deleteLastNode(list);
+    if (value == NULL) {
+        printf("Возникла ошибка при удалении ноды\n");
+        return false;
+    }
+    if (strcmp(value, "")) {
+        testResult = false;
+    }
+
+    value = deleteLastNode(list);
+    if (value == NULL) {
+        printf("Возникла ошибка при удалении ноды\n");
+        return false;
+    }
+    if (strcmp(value, "abc")) {
+        testResult = false;
+    }
+    clear(list);
     return testResult;
 }
 
@@ -12,10 +99,7 @@ int main() {
     if (!test()) {
         printf("Тесты провалены\n");
         return -1;
-    } else {
-        printf("Тесты пройдены\n");
     }
-
-
+    printf("Тесты пройдены\n");
     return 0;
 }
