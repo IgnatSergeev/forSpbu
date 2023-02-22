7bf5069873c579581cb7163e7a1bbaa14f286502
IgnatSergeev/laptop <ignatii.f.sergeev@gmail.com>
2023-02-23 01:48:31 +0300

    Added tests

--- C#/forSpbu/BubbleSort/Program.cs
+++ C#/forSpbu/BubbleSort/Program.cs
@@ -4,7 +4,23 @@ namespace BubbleSort
 {
     class Program
     {
-        static void BubbleSort(int[] array)
+        static bool test()
+        {
+            int[] array = { 12, 34, 5, 1 };
+            BubbleSort(array);
+
+            int[] sortedArray = { 1, 5, 12, 34 };
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] != sortedArray[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        public static void BubbleSort(int[] array)
         {
             int size = array.Length;
             for (int i = 0; i < size; i++)
@@ -21,16 +37,27 @@ namespace BubbleSort
             }
         }
         
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            if (!test())
+            {
+                Console.WriteLine("Тесты провалены");
+                return -1;
+            }
+            Console.WriteLine("Тесты пройдены\n");
+            
+            Console.WriteLine("Введите элементы массива, который хотите отсортировать, через пробел");
             int[] array = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
             BubbleSort(array);
             
+            Console.WriteLine("Вот отсортированный массив:");
             for (int i = 0; i < array.Length; i++)
             {
                 Console.Write(array[i]);
                 Console.Write(" ");
             }
+
+            return 0;
         }
     }
 }
