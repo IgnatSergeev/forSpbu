b2f9c27d26ff3c1420d63d6beb6ebdfc3c051d7e
IgnatSergeev/Desktop <ignatii.f.sergeev@gmail.com>
2023-03-18 00:06:54 +0300

    Added user interface and started encode realisation

---
+++ C#/forSpbu/LZW/LZW.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace LZW;
+
+public static class Lzw
+{
+    public static void Encode(FileStream fileStream, FileStream outFile, Trie.Trie trie)
+    {
+        if (fileStream == null)
+        {
+            throw new ArgumentNullException(nameof(fileStream));
+        }
+        if (trie == null)
+        {
+            throw new ArgumentNullException(nameof(trie));
+        }
+
+        const int maxByte = (2 << (sizeof(byte) * 8 - 1)) - 1;
+        for (int i = 0; i <= maxByte; i++)
+        {
+            char symbol = (char)i;
+            trie.AddChar("", symbol, i);
+        }
+
+        int code = maxByte + 1;
+        var stack = new Stack<char>();
+        int fileByte = fileStream.ReadByte();
+        while (fileByte != -1)
+        {
+            var symbol = (char)fileByte;
+            
+            var phrase = stack.ToArray();
+            if (!trie.Contains(phrase + symbol))
+            {
+                var byteArray = BitConverter.GetBytes(trie.GetCode(phrase));
+                outFile.Write(byteArray);
+                trie.AddChar(phrase, symbol, code++);
+                stack = new Stack<char>();
+                stack.Push(symbol);
+            }
+
+            fileByte = fileStream.ReadByte();
+        }
+
+        if (stack.Count <= 0) return;
+        {
+            var phrase = new string(stack.ToArray());
+            var byteArray = BitConverter.GetBytes(trie.GetCode(phrase));
+            outFile.Write(byteArray);
+        }
+    }
+    
+    public static void Decode(FileStream fileStream, FileStream outFile, Trie.Trie trie)
+    {
+        if (fileStream == null)
+        {
+            throw new ArgumentNullException(nameof(fileStream));
+        }
+        if (trie == null)
+        {
+            throw new ArgumentNullException(nameof(trie));
+        }
+        
+        
+    }
+}
---
+++ C#/forSpbu/LZW/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+if (args.Length != 2)
+{
+    Console.WriteLine("Wrong number of arguments");
+    return 0;
+}
+
+var pathToFile = args[0];
+var key = args[1];
+if (string.IsNullOrEmpty(pathToFile) || string.IsNullOrEmpty(key))
+{
+    Console.WriteLine("Wrong(null or empty) arguments");
+    return 0;
+}
+
+try
+{
+    using var fileStream = File.Open(pathToFile, FileMode.Open);
+    using var outFile = File.Create(pathToFile + ".zip");
+    var trie = new Trie.TrieRealisation();
+    
+    switch (key)
+    {
+        case "-c":
+            LZW.Lzw.Encode(fileStream, outFile, trie);
+            break;
+        case "-u":
+            LZW.Lzw.Decode(fileStream, outFile, trie);
+            break;
+        default:
+            Console.WriteLine("Wrong key argument");
+            break;
+    }
+}
+catch (IOException)
+{
+    Console.WriteLine("File not found");
+}
+
+return 0;
+
