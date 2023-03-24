003584978ac4d126ae90984675f6277a130975df
IgnatSergeev/Desktop <ignatii.f.sergeev@gmail.com>
2023-03-24 18:19:55 +0300

    Added comments to BufferedFileStream class

--- C#/forSpbu/LZW/BufferedFileStream.cs
+++ C#/forSpbu/LZW/BufferedFileStream.cs
@@ -1,10 +1,19 @@
 ﻿namespace LZW;
 
+/// <summary>
+/// Class for working with file stream using buffered output
+/// </summary>
 public class BufferedFileStream : IDisposable
 {
+    /// <summary>
+    /// Creates buffered file stream with creating a new file by the path
+    /// </summary>
+    /// <param name="path">path of the file</param>
+    /// <returns>buffered file stream object</returns>
+    /// <exception cref="ArgumentNullException">if given path is null or empty</exception>
     public static BufferedFileStream Create(string path)
     {
-        if (path == null)
+        if (string.IsNullOrEmpty(path))
         {
             throw new ArgumentNullException(nameof(path));
         }
@@ -15,9 +24,16 @@ public class BufferedFileStream : IDisposable
         return result;
     }
     
+    /// <summary>
+    /// Creates buffered file stream using file by the path
+    /// </summary>
+    /// <param name="path">path of the file</param>
+    /// <param name="mode">FileMode of working with file</param>
+    /// <returns>buffered file stream object</returns>
+    /// <exception cref="ArgumentNullException">if given path is null or empty</exception>
     public static BufferedFileStream Open(string path, FileMode mode)
     {
-        if (path == null)
+        if (string.IsNullOrEmpty(path))
         {
             throw new ArgumentNullException(nameof(path));
         }
@@ -53,6 +69,12 @@ public class BufferedFileStream : IDisposable
         return result;
     }
 
+    /// <summary>
+    /// Read given number of bits from file stream
+    /// </summary>
+    /// <param name="numOfBits">number of bit to read</param>
+    /// <returns>Tuple of array of bits and bool defining was the reading interrupted by the end of the file</returns>
+    /// <exception cref="ArgumentOutOfRangeException">if given number of bits less or equal to zero</exception>
     public (bool[], bool) ReadBits(int numOfBits)
     {
         if (numOfBits <= 0)
@@ -74,7 +96,7 @@ public class BufferedFileStream : IDisposable
         }
 
         int numOfBitsToRead = numOfBits - numOfAlreadyReadBits;
-        int numbOfBytesToRead = numOfBitsToRead / byteSize;
+        int numbOfBytesToRead = numOfBitsToRead / ByteSize;
         for (int i = 0; i < numbOfBytesToRead; i++)
         {
             var currentByte = _fileStream?.ReadByte();
@@ -83,14 +105,14 @@ public class BufferedFileStream : IDisposable
                 return (bits, true);
             }
 
-            for (int j = 0; j < byteSize; j++)
+            for (int j = 0; j < ByteSize; j++)
             {
-                int index = numOfAlreadyReadBits + (i + 1) * byteSize - j - 1;
+                int index = numOfAlreadyReadBits + (i + 1) * ByteSize - j - 1;
                 bits[index] = (currentByte >> j) % 2 != 0;
             }
         }
 
-        if (numOfBitsToRead % byteSize != 0)
+        if (numOfBitsToRead % ByteSize != 0)
         {
             var currentByte = _fileStream?.ReadByte();
             if (currentByte == -1)
@@ -98,21 +120,26 @@ public class BufferedFileStream : IDisposable
                 return (bits, true);
             }
 
-            for (int i = 0; i < numOfBitsToRead % byteSize; i++)
+            for (int i = 0; i < numOfBitsToRead % ByteSize; i++)
             {
-                int index = bits.Length - numOfBitsToRead % byteSize + i;
-                bits[index] = (currentByte >> (byteSize - 1 - i)) % 2 != 0;
+                int index = bits.Length - numOfBitsToRead % ByteSize + i;
+                bits[index] = (currentByte >> (ByteSize - 1 - i)) % 2 != 0;
             }
 
-            _readBufferSize = byteSize - (numOfBitsToRead % byteSize);
+            _readBufferSize = ByteSize - (numOfBitsToRead % ByteSize);
             for (int i = 0; i < _readBufferSize; i++)
             {
-                _readBuffer[i] = (currentByte >> (byteSize - 1 - numOfBitsToRead % byteSize - i)) % 2 != 0;
+                _readBuffer[i] = (currentByte >> (ByteSize - 1 - numOfBitsToRead % ByteSize - i)) % 2 != 0;
             }
         }
         return (bits, false);
     }
 
+    /// <summary>
+    /// Writes given bits to file stream
+    /// </summary>
+    /// <param name="bitsToWrite">array of bits to write</param>
+    /// <exception cref="ArgumentNullException">if given array is null</exception>
     public void WriteBits(bool[] bitsToWrite)
     {
         if (bitsToWrite == null)
@@ -129,7 +156,7 @@ public class BufferedFileStream : IDisposable
 
     private void WriteBit(bool b)
     {
-        if (_firstFreeWriteBufferIndex < byteSize - 1)
+        if (_firstFreeWriteBufferIndex < ByteSize - 1)
         {
             _writeBuffer[_firstFreeWriteBufferIndex++] = b;
         }
@@ -141,10 +168,10 @@ public class BufferedFileStream : IDisposable
         }
     }
 
-    private const int byteSize = 8;
+    private const int ByteSize = 8;
     private FileStream? _fileStream;
-    private readonly bool[] _writeBuffer = new bool[byteSize];
-    private readonly bool[] _readBuffer = new bool[byteSize];
+    private readonly bool[] _writeBuffer = new bool[ByteSize];
+    private readonly bool[] _readBuffer = new bool[ByteSize];
     private int _readBufferSize;
     private int _firstFreeWriteBufferIndex;
 }
