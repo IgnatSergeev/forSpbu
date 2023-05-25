b0076b8a1cb579dc890dbcf917de67f2c675e113
IgnatSergeev <ignatii.f.sergeev@gmail.com>
2023-05-26 01:27:51 +0300

    Finished core

--- C#/forSpbu/Calculator/CalculatorCore.cs
+++ C#/forSpbu/Calculator/CalculatorCore.cs
@@ -10,7 +10,8 @@ public class CalculatorCore
         Div,
         Mod,
         Clear,
-        Equal
+        Equal,
+        None
     }
 
     private enum State
@@ -20,7 +21,8 @@ public class CalculatorCore
         SecondOperandNumber,
         SecondOperandComma,
         Operation,
-        Error
+        Error,
+        None
     }
 
     private Operation StringGetOperation(string operationString)
@@ -37,100 +39,187 @@ public class CalculatorCore
             _ => throw new ArgumentOutOfRangeException(nameof(operationString), operationString, null)
         };
     }
-    
-    private double StringGetNumber(string numberString)
-    {
-        return numberString switch
-        {
-            "0" => 0,
-            "1" => 1,
-            "2" => 2,
-            "3" => 3,
-            "4" => 4,
-            "5" => 5,
-            "6" => 6,
-            "7" => 7,
-            "8" => 8,
-            "9" => 9,
-            _ => throw new ArgumentOutOfRangeException(nameof(numberString), numberString, null)
-        };
-    }
-    
+
     public void TakeNumberToken(string numberString)
     {
+        if (numberString is not ("0" or "1" or "2" or "3" or "4" or "5" or "6" or "7" or "8" or "9" or ","))
+        {
+            throw new ArgumentOutOfRangeException(nameof(numberString), numberString, null);
+        }
+        /// запятая после числа с запятой, равно  это очищение
         switch (_state)
         {
             case State.Error:
-                _number = numberString;
-                _expression = "";
-                _state = State.FirstOperandNumber;
+                if (numberString != ",")
+                {
+                    _number = numberString;
+                    _expression = "";
+                    _state = State.FirstOperandNumber;
+                }
                 break;
             case State.FirstOperandComma:
+                if (numberString == ",")
+                {
+                    _state = State.Error;
+                    break;
+                }
+                
                 _number += numberString;
                 _state = State.FirstOperandNumber;
                 break;
             case State.FirstOperandNumber:
+                if (hasComma && numberString == ",")
+                {
+                    _state = State.Error;
+                    break;
+                }
+
+                hasComma = numberString == ",";
                 _number += numberString;
                 break;
             case State.SecondOperandComma:
+                if (numberString == ",")
+                {
+                    _state = State.Error;
+                    break;
+                }
+                
                 _number += numberString;
                 _state = State.SecondOperandNumber;
                 break;
             case State.SecondOperandNumber:
+                if (hasComma && numberString == ",")
+                {
+                    _state = State.Error;
+                    break;
+                }
+                
+                hasComma = numberString == ",";
                 _number += numberString;
                 break;
             case State.Operation:
                 _number = numberString;
+                if (_operation == Operation.Equal)
+                {
+                    _operation = Operation.None;
+                    _expression = "";
+                    _state = State.FirstOperandNumber;
+                }
+                
+                _state = State.SecondOperandNumber;
+                break;
+            case State.None:
+                _number = numberString;
+                _expression = "";
                 _state = State.FirstOperandNumber;
                 break;
-            default:
-                throw new ArgumentOutOfRangeException(nameof(_state), _state, null);
+        }
+    }
+
+    private void ApplyOperation()
+    {
+        switch (_operation)
+        {
+            case Operation.Add:
+                firstOperand += secondOperand;
+                break;
+            case Operation.Sub:
+                firstOperand -= secondOperand;
+                break;
+            case Operation.Mul:
+                firstOperand *= secondOperand;
+                break;
+            case Operation.Div:
+                firstOperand /= secondOperand;
+                break;
+            case Operation.Mod:
+                firstOperand %= secondOperand;
+                break;
         }
     }
     
     public void TakeOperationToken(string operationString)
     {
         var operation = StringGetOperation(operationString);
+        if (operation == Operation.Clear)
+        {
+            _state = State.None;
+            _operation = Operation.None;
+        }
+        
         switch (_state)
         {
             case State.Error:
                 break;
             case State.FirstOperandComma:
                 _state = State.Operation;
-                if (operation == Operation.Clear)
-                {
-                    
-                }
                 _number = _number.Remove(_number.Length - 1);
-                if (!double.TryParse(_number, out firstOperand))
-                {
-                    throw new ArgumentOutOfRangeException(nameof(_number), _number, null);;
-                }
-                _expression += _number + operationString;
-                break;
-            case State.FirstOperandNumber:
-                _state = State.Operation;
-                _expression += _number + operationString;
-                if (!double.TryParse(_number, out firstOperand))
-                {
-                    throw new ArgumentOutOfRangeException(nameof(_number), _number, null);;
-                }
-                break;
-            case State.SecondOperandComma:
-                _state = State.Operation;
-                _number = _number.Remove(_number.Length - 1);
+                
                 if (!double.TryParse(_number, out firstOperand))
                 {
                     throw new ArgumentOutOfRangeException(nameof(_number), _number, null);;
                 }
                 
+                _operation = operation;
+                _expression = firstOperand + " " + operationString;
+                break;
+            case State.FirstOperandNumber:
+                _state = State.Operation;
+                if (!double.TryParse(_number, out firstOperand))
+                {
+                    throw new ArgumentOutOfRangeException(nameof(_number), _number, null);;
+                }
+                
+                _operation = operation;
+                _expression = firstOperand + " " + operationString;
+                break;
+            case State.SecondOperandComma:
+                _state = State.Operation;
+                _number = _number.Remove(_number.Length - 1);
+                if (!double.TryParse(_number, out secondOperand))
+                {
+                    throw new ArgumentOutOfRangeException(nameof(_number), _number, null);
+                }
+
+                ApplyOperation();
+                _operation = operation;
+
+                _expression = firstOperand + " " + operationString;
+                _number = "";
+                
+                break;
+            case State.SecondOperandNumber:
+                _state = State.Operation;
+                if (!double.TryParse(_number, out secondOperand))
+                {
+                    throw new ArgumentOutOfRangeException(nameof(_number), _number, null);
+                }
+
+                ApplyOperation();
+                _operation = operation;
+
+                _expression = firstOperand + " " + operationString;
+                _number = "";
+                
+                break;
+            case State.Operation:
+                _operation = operation;
+                _expression = firstOperand + " " + operationString;
+                break;
+            case State.None:
+                _state = State.Operation;
+                _operation = operation;
+                _expression = "0 " + operationString;
+                break;
         }
     }
 
     private double firstOperand = 0;
     private double secondOperand = 0;
-    private State _state = State.Error;
+    private Operation _operation = Operation.None;
+    private State _state = State.None;
     private string _number = "0";
+    private bool hasComma = false;
     private string _expression = "";
     private const double Delta = 0.0001;
 }
