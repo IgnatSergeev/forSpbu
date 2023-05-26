ff7afe01e13055d07674cc94215deea8e79cdd72
IgnatSergeev/laptop <ignatii.f.sergeev@gmail.com>
2023-05-26 14:18:31 +0300

    Added realisation

---
+++ C#/forSpbu/RunningButton/RunningButtonForm.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace RunningButton;
+
+partial class RunningButtonForm
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        this.Size = new Size(400, 400);
+        this.MinimumSize = new Size(300, 400);
+        this.Text = "RunningButton";
+        this.Controls.Add(_runningButton);
+        
+        _runningButton.BackColor = Color.Gray;
+        _runningButton.TextAlign = ContentAlignment.MiddleCenter;
+        _runningButton.Text = "Button";
+        _runningButton.ForeColor = Color.Azure;
+        _runningButton.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Pixel);
+        _runningButton.MouseEnter += OnEnter;
+        _runningButton.Click += OnClick;
+        _runningButton.Location = new Point(0, 0);
+        _runningButton.Anchor = AnchorStyles.Top | AnchorStyles.Left;
+        _runningButton.Size = new Size(100, 100);
+    }
+
+    #endregion
+
+    private Button _runningButton = new Button();
+    private Position _buttonPosition = Position.TopLeft;
+}
---
+++ C#/forSpbu/RunningButton/RunningButtonForm.cs
@@ -0,0 +1,59 @@
+namespace RunningButton;
+
+public partial class RunningButtonForm : Form
+{
+    public RunningButtonForm()
+    {
+        InitializeComponent();
+    }
+
+    enum Position
+    {
+        TopLeft,
+        TopRight,
+        BottomRight,
+        BottomLeft
+    }
+    
+    private void OnEnter(object? sender, EventArgs eventArgs)
+    {
+        if (sender is not Button senderButton)
+        {
+            return;
+        }
+
+        switch (_buttonPosition)
+        {
+            case Position.TopLeft:
+                _buttonPosition += 1;
+                senderButton.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+                senderButton.Location = new Point(Size.Width - senderButton.Size.Width, 0);
+                break;
+            case Position.TopRight:
+                _buttonPosition += 1;
+                senderButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+                senderButton.Location = new Point(Size.Width - senderButton.Size.Width, Size.Height - senderButton.Size.Height);
+                break;
+            case Position.BottomRight:
+                _buttonPosition += 1;
+                senderButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+                senderButton.Location = new Point(0, Size.Height - senderButton.Size.Height);
+                break;
+            case Position.BottomLeft:
+                _buttonPosition = 0;
+                senderButton.Anchor = AnchorStyles.Top | AnchorStyles.Left;
+                senderButton.Location = new Point(0, 0);
+                break;
+        }
+    }
+    
+    private void OnClick(object? sender, EventArgs eventArgs)
+    {
+        if (sender is not Button senderButton)
+        {
+            return;
+        }
+        
+        Close();
+    }
+}
