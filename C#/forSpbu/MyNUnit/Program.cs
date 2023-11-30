8b7e3bf6ee02d4e50827d82eb55014fe60440694
IgnatSergeev <ignat.sergeev@softcom.su>
2023-11-30 05:31:08 +0300

    Finished

--- C#/forSpbu/MyNUnit/Program.cs
+++ C#/forSpbu/MyNUnit/Program.cs
@@ -1,39 +1,32 @@
 using System.Reflection;
+using MyNUnit;
 
-namespace MyNUnit;
-
-internal static class Program
+if (args.Length != 1 || string.IsNullOrEmpty(args[0]))
 {
-    public static void Main(string[] argv)
-    {
-        if (argv.Length != 1 || string.IsNullOrEmpty(argv[0]))
-        {
-            Console.WriteLine("Incorrect argument, should be project assembly(binary or dll) path");
-        }
-
-        var assembly = Assembly.LoadFile(argv[0]);
-
-        var runResults = Tester.TestAssembly(assembly);
-        var summary = runResults.Aggregate<TestResult, (int ok, int errors, int ignored)>((0, 0, 0), (summary, result) =>
-        {
-            if (result.Ignored)
-            {
-                summary.ignored++;
-            }
-            if (result.Passed)
-            {
-                summary.ok++;
-            }
-            else
-            {
-                summary.errors++;
-            }
-            
-            Console.WriteLine(result.ToString());
-
-            return summary;
-        });
-        
-        Console.WriteLine($"Summary: {summary.ok} ok, {summary.errors} failed, {summary.ok} ignored");
-    }
+    Console.WriteLine("Incorrect argument, should be project assembly(binary or dll) path");
 }
+
+var assembly = Assembly.LoadFile(args[0]);
+
+var results = Tester.TestAssembly(assembly);
+var summary = results.Aggregate<TestResult, (int ok, int errors, int ignored)>((0, 0, 0), (summary, result) =>
+{
+    if (result.Ignored)
+    {
+        summary.ignored++;
+    }
+    if (result.Passed)
+    {
+        summary.ok++;
+    }
+    else
+    {
+        summary.errors++;
+    }
+            
+    Console.WriteLine(result.ToString());
+
+    return summary;
+});
+        
+Console.WriteLine($"Summary: {summary.ok} ok, {summary.errors} failed, {summary.ok} ignored");
