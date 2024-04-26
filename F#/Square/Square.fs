d740d5f9608d5abda16faebd6e527009e9a1a60a
ignat.sergeev <ignat.sergeev@softcom.su>
2024-04-26 16:58:59 +0300

    Added Square

---
+++ F#/Square/Square.fs
@@ -0,0 +1,28 @@
+﻿
+module Square
+
+/// Generates string with star square
+let genStarSquare n =
+    let rec replicateStr str times = 
+        match times with
+        | 1 -> str
+        | _ ->
+            let prevStr = (replicateStr str (times - 1)):string
+            prevStr.Insert(0, str)
+    
+    let rec genSquare i =
+        match i with
+        | 1 ->
+            replicateStr "*" n
+        | _ ->
+            let prev = genSquare (i - 1)
+            if i = n then
+                prev.Insert(prev.Length, "\n" + replicateStr "*" n)
+            else
+                prev.Insert(prev.Length, "\n*" + replicateStr " " (n - 2) + "*")
+    
+    if n > 0 then
+        genSquare n
+    else
+        ""
+   
