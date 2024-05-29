848a76d072d63c0909da69902345964e25bb9652
IgnatSergeev <ignat.sergeev@softcom.su>
2024-05-29 22:10:08 +0300

    Added realisations

---
+++ F#/ParseTree/ParseTree.fs
@@ -0,0 +1,25 @@
+﻿module ParseTree
+
+type MaybeBuilder() =
+    member this.Bind(m, f) = Option.bind f m
+    member this.Return(x) =
+        Some x
+    member this.ReturnFrom(m) =
+        m
+
+let maybe = new MaybeBuilder()
+
+type ParseTree<'T> = 
+    | Value of 'T
+    | Add of ParseTree<'T> * ParseTree<'T>
+    | Subtract of ParseTree<'T> * ParseTree<'T>
+    | Divide of ParseTree<'T> * ParseTree<'T>
+    | Multiply of ParseTree<'T> * ParseTree<'T>
+
+let rec calulateParseTree tree = 
+    match tree with
+    | Value value -> value
+    | Add (lhs, rhs) -> calulateParseTree lhs + calulateParseTree rhs
+    | Subtract (lhs, rhs) -> calulateParseTree lhs - calulateParseTree rhs
+    | Multiply (lhs, rhs) -> calulateParseTree lhs * calulateParseTree rhs
+    | Divide (lhs, rhs) -> calulateParseTree lhs / calulateParseTree rhs
