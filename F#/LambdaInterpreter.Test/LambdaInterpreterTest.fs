b7e54125e5be4f90e1360a82fda51a40de6d54e1
IgnatSergeev <ignat.sergeev@softcom.su>
2024-05-31 15:59:27 +0300

    Added comments and tests

--- F#/LambdaInterpreter.Test/LambdaInterpreterTest.fs
+++ F#/LambdaInterpreter.Test/LambdaInterpreterTest.fs
@@ -9,21 +9,33 @@ let KStar = Abstr("y", Abstr("x", Var("x")))
 let I = Abstr("x", Var("x"))
 
 [<Test>]
-let VarTest () =
+let getAllNamesTest () =
+    App(Abstr("a", Abstr("x", App(Var("a"), Var("x")))), Var("b")) |> getAllNames |> should equal (Set.ofList [ "a"; "x"; "b"])
+
+[<Test>]
+let getNameTest () =
+    App(Abstr("a", Abstr("b", App(App(Var("a"), Var("b1")), Var("b")))), Var("b")) |> generateName "b" |> should equal "b2"
+
+[<Test>]
+let alphaTest () =
+    Abstr("b", App(Var("a"), Var("b"))) |> alphaTransform |> should equal (Abstr("b1", App(Var("a"), Var("b1"))))
+
+[<Test>]
+let betaTest () =
+    App(Abstr("a", Abstr("b", App(App(Var("a"), Var("b1")), Var("b")))), Var("b")) |> betaReduce |> should equal (Abstr("b2", App(App(Var("b"), Var("b1")), Var("b2"))))
+
+[<Test>]
+let varNormalizeTest () =
     Var("a") |> normalize |> should equal (Var("a"))
 
 [<Test>]
-let AbstrTest () =
+let INormalizeTest () =
     I |> normalize |> should equal I
+
+[<Test>]
+let renameNormalizeTest () =
+    App(Abstr("a", Abstr("b", App(Var("a"),Var("b")))), Var("b")) |> normalize |> should equal (Abstr("b1", App(Var("b"),Var("b1"))))
 
 [<Test>]
 let KIKStarTest () =
     App(K, I) |> normalize |> should equal KStar
-
-[<Test>]
-let SKKTest () =
-    App(App(S, K), K) |> normalize |> should equal I
-
-[<Test>]
-let RenameTest () =
-    App(Abstr("a", Abstr("b", App(Var("a"),Var("b")))), Var("b")) |> normalize |> should equal I
