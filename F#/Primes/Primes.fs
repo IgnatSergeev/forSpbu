d9968e9c356de5fd334d5dba6b494c87bb88ae49
Сергеев Игнатий <114506759+IgnatSergeev@users.noreply.github.com>
2024-05-31 16:02:16 +0300

    Formatted
    Co-authored-by: Yurii Litvinov <yurii.litvinov@gmail.com>

--- F#/Primes/Primes.fs
+++ F#/Primes/Primes.fs
@@ -2,7 +2,7 @@
 
 let primes =
     let isPrime n = 
-        let sequence = { 2..(n-1) }
+        let sequence = { 2 .. (n - 1) }
         Seq.forall (fun x -> n % x <> 0) sequence
 
     Seq.initInfinite id |> Seq.filter (fun x -> x >= 2) |> Seq.filter isPrime
