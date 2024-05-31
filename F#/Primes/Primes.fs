6827958a72cb14de5cbe3289f4e75b15a090e71d
IgnatSergeev <ignat.sergeev@softcom.su>
2024-05-31 16:05:18 +0300

    Fixed review comments

--- F#/Primes/Primes.fs
+++ F#/Primes/Primes.fs
@@ -1,8 +1,11 @@
 ﻿module Primes 
 
+/// <summary>
+/// Generates infinite sequence of prime numbers
+/// </summary>
 let primes =
     let isPrime n = 
-        let sequence = { 2 .. (n - 1) }
+        let sequence = { 2 .. (n |> float |> sqrt |> int) }
         Seq.forall (fun x -> n % x <> 0) sequence
 
     Seq.initInfinite id |> Seq.filter (fun x -> x >= 2) |> Seq.filter isPrime
